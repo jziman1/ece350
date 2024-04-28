4b683e2bddcbcfed192c0ab21d73e6f7406c8d45
Jonty Ziman <47163854+jziman1@users.noreply.github.com>
2024-04-27 23:38:21 -0400

    Add files via upload

--- RAM.v
+++ RAM.v
@@ -1,12 +1,3 @@
-// Switch: MemoryArray[1]
-// LED:  MemoryArray[2]
-// Motor enable: MemoryArray[3]
-// Servo: MemoryArray[4]
-// Numpad: MemoryArray[5]
-// Servo Time: MemoryArray[6]
-// Continue: MemoryArray[7]
-
-
 `timescale 1ns / 1ps
 module RAM #( parameter DATA_WIDTH = 32, ADDRESS_WIDTH = 12, DEPTH = 20) (
     input wire                     clk,
@@ -17,11 +8,8 @@ module RAM #( parameter DATA_WIDTH = 32, ADDRESS_WIDTH = 12, DEPTH = 20) (
     input wire [15:0] SW,
     input wire [3:0] num,
     input wire [31:0] remainder,
-    output reg servo_enable,
     output reg [15:0] LED,
-    output reg motor_enable,
     output reg remainder_led,
-    output reg [31:0] servo_time,
     output reg mode);
     
     reg[DATA_WIDTH-1:0] MemoryArray[0:DEPTH-1];
@@ -46,14 +34,9 @@ module RAM #( parameter DATA_WIDTH = 32, ADDRESS_WIDTH = 12, DEPTH = 20) (
         end else begin
             dataOut <= MemoryArray[addr];
         end
-     //   MemoryArray[8] <= SW_final;
         LED[15:12] <= MemoryArray[5][3:0];
         LED[11:0] <= MemoryArray[6][11:0];
-        //LED <= MemoryArray[10][15:0];
-       // motor_enable <= MemoryArray[3][1:0]; // 1 to enable motor, 0 to disable
-        //servo_enable <= MemoryArray[4][1:0]; // 1 to enable servo, 0 to disable
         MemoryArray[5] <= num;
-        //servo_time <= MemoryArray[6][31:0];
         MemoryArray[10] <= remainder;
         remainder_led <= MemoryArray[7][1:0]; 
         mode <= MemoryArray[11][1:0];       
--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# Processor
-## NAME (NETID)
+# Final Project
+## New Files
+MotorPWMSerializer.v controls the motor speed through a PWM signal.
+ServoPWMSerializer.v controls the continuous servo speed and direciton through a PWM signal.
+keypad.v allows us to read in values from the keypad.
+control.v allows for a custom instruction that activates the servo then activates the motor.
 
-## Description of Design
-
-## Bypassing
-
-## Stalling
-
-## Optimizations
-
-## Bugs
+## Edited files
+Wrapper.v was editted to allow for I/O
+RAM was editted to allow for I/O
+Processor was editted to include a custom instruction and to route the division remainder to RAM
